982d550682d4f0b3aee50c3bb3eb9af837e58e03
chanjun.park <chanjun.park@navercorp.com>
2021-12-17 21:07:04 +0900

    lottoResult 테이블에 유니크 속성 추가, api 모델에서 db 모델로 변경하는 메소드 추가

--- app/src/main/java/com/jingom/composelotto/db/model/LottoResult.kt
+++ app/src/main/java/com/jingom/composelotto/db/model/LottoResult.kt
@@ -3,17 +3,19 @@ package com.jingom.composelotto.db.model
 import androidx.annotation.VisibleForTesting
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
+import com.jingom.composelotto.api.model.DHLottoResponseBody
 
 @VisibleForTesting
 const val INVALID = -1
 
-@Entity(tableName = "lotto_result")
+@Entity(tableName = "lotto_result", indices = [Index(value = ["lottery_no"], unique = true)])
 data class LottoResult(
 	@PrimaryKey(autoGenerate = true)
 	val id: Long = 0,
 
-	val day: String? = "",
+	val day: String = "",
 
 	@ColumnInfo(name = "total_sell_amount")
 	val totalSellAmount: Long = INVALID.toLong(),
@@ -39,4 +41,22 @@ data class LottoResult(
 
 	@ColumnInfo(name = "lottery_no")
 	val lotteryNo: Int = INVALID
-)
+) {
+	companion object {
+		fun from(dhLottoResponseBody: DHLottoResponseBody) = LottoResult(
+			day = dhLottoResponseBody.dayOfLottery ?: "",
+			totalSellAmount = dhLottoResponseBody.totalSellAmount,
+			firstWinAmount = dhLottoResponseBody.firstWinAmount,
+			firstPrizeWinnerCount = dhLottoResponseBody.firstPrizeWinnerCount,
+			firstAccumulatedAmount = dhLottoResponseBody.firstAccumulatedAmount,
+			no1 = dhLottoResponseBody.no1,
+			no2 = dhLottoResponseBody.no2,
+			no3 = dhLottoResponseBody.no3,
+			no4 = dhLottoResponseBody.no4,
+			no5 = dhLottoResponseBody.no5,
+			no6 = dhLottoResponseBody.no6,
+			bonusNo = dhLottoResponseBody.bonusNo,
+			lotteryNo = dhLottoResponseBody.lotteryNo
+		)
+	}
+}
